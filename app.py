9fa888805fae1b779f5573dfb99bc2424e9a842a
mina <00qpqqp@gmail.com>
2023-06-15 15:21:49 +0900

    updated

--- app.py
+++ app.py
@@ -1,189 +1,50 @@
-# -*- coding:UTF-8 -*-
-import matplotlib.pyplot as plt
-plt.rc('font', family='S-Core Dream')
-# 데이터 불러오기
+# -*- coding:utf-8 -*-
+import streamlit as st
+from streamlit_option_menu import option_menu
 
-# [40]
-import pandas as pd
+from gallup import run_gallup
+from election import run_election
+from PIL import Image
 
-df = pd.read_excel("data/이념적성향_변동.xlsx", )
-df['연도'] = df['연도'].astype('str')
-df['구분'] = df['구분'].replace({
-    "매우보수적": "보수",
-    "다소보수적": "보수",
-    "매우진보적": "진보",
-    "다소진보적": "진보",
-    "중도적" : "중도"
-})
+import base64
+from pathlib import Path
 
-df2 = df.groupby(['세대', '연도', '구분'])['비율'].agg('sum').reset_index()
-df2
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+def img_to_html(img_path):
+    img_html = "<img src='data:image/png;base64,{}' class='img-fluid' width='50' height='50'>".format(
+      img_to_bytes(img_path)
+    )
+    return img_html
 
-# [41]
-df20s = df2[df2['세대'] == '19~29세']
-df30s = df2[df2['세대'] == '30~39세']
-# 20대와 30대의 비율 차이
-# [89]
-import seaborn as sns
+def main():
+    with st.sidebar:
+        selected = option_menu("대시보드 메뉴", ['홈', '갤럽여론조사', '20대 대통령선거'],
+                               icons=['house', 'file-bar-graph', 'graph-up-arrow'], menu_icon="cast", default_index=0)
+    if selected == "홈":
+        st.markdown("<h1 style='text-align: center; color: black;'>대통령 선거 데이터 분석</h1>", unsafe_allow_html=True)
 
-custom_palette = ['#E61E2B', '#808080', '#004EA1']
+        st.markdown("<h2 style='text-align: center; color: gray;'>10-30대는 어떻게 움직였는가? </h2>"
+                    "<p style='text-align: center; color: grey;'>"+img_to_html('img/figure01.png')+"</p>",
+                    unsafe_allow_html=True)
+        st.markdown("# 갤럽 데이터 수집\n"
+                    "- 데이터 수집항목 : 2020년 1월 2째주 ~ 2022년 12월 3째주\n"
+                    "- 날짜 : 연도, 월, 주\n"
+                    "- 정당 : 국민의힘 / 더불어민주당 / 정의당 / 무당층\n"
+                    "- 무당층 : 기타 정당과 부동층을 포함\n"
+                    "- 지역 : 서울, 인천/경기, 대전/세종/충청, 광주/전라, 대구/경북, 부산/울산\n"
+                    "- 성별 : 남자, 여자\n"
+                    "- 연령 : 10대~30대\n"
+                    "- 웹싸이트 : https://www.gallup.co.kr/")
 
-fig, ax = plt.subplots(figsize=(10, 6), ncols=2)
-sns.lineplot(data = df20s, x = '연도', y = '비율', hue = '구분', ax = ax[0], palette=custom_palette)
-ax[0].set_title("19~29세의 이념성향 추이", size=22)
-ax[0].set_ylim(0, 100)
+    elif selected == "갤럽여론조사":
+        run_gallup()
+    elif selected == "20대 대통령선거":
+        run_election()
+    else:
+        print("error...")
 
-sns.lineplot(data = df30s, x = '연도', y = '비율', hue = '구분', ax = ax[1], palette=custom_palette)
-ax[1].set_title("30대의 이념성향 추이", size=22)
-ax[1].set_ylim(0, 100)
-plt.legend(loc="best")
-plt.show()
-
-# 갤럽정당지지도
-# [75]
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-df = pd.read_excel('data/정당지지도_데이터.xlsx')
-df['date'] = pd.to_datetime(df[['연도', '주']].astype(str).agg('-W'.join, axis=1) + '-5', format='%Y-W%W-%w')
-df
-
-# [77]
-df['정당'].unique()
-array(['국민의힘', '더불어민주당', '정의당', '무당층'], dtype=object)
-# [86]
-custom_palette = ['#E61E2B', '#004EA1', '#FFED00', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '10~20', hue = '정당', palette=custom_palette)
-ax.set_title("10-20대")
-plt.savefig("output/10-20.png", dpi='figure')
-plt.show()
-
-# [87]
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '30대', hue = '정당', palette=custom_palette)
-ax.set_title("30대")
-plt.savefig("output/30.png", dpi='figure')
-plt.show()
-#
-# 성별에 따른 정당 지지도의 흐름
-# [95]
-custom_palette = ['#E61E2B', '#004EA1', '#FFED00', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '남자', hue = '정당', palette=custom_palette)
-ax.set_title("정당별 남자 지지도", size=22)
-plt.savefig("output/남자.png", dpi='figure')
-plt.show()
-
-# [96]
-custom_palette = ['#E61E2B', '#004EA1', '#FFED00', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '여자', hue = '정당', palette=custom_palette)
-ax.set_title("정당별 여자 지지도", size=22)
-plt.savefig("output/여자.png", dpi='figure')
-plt.show()
-
-# 1년 뒤
-# [101]
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-df = pd.read_excel('data/정당지지도_데이터_2023.xlsx')
-df['date'] = pd.to_datetime(df[['연도', '주']].astype(str).agg('-W'.join, axis=1) + '-5', format='%Y-W%W-%w')
-df
-
-# [108]
-custom_palette = ['#E61E2B', '#004EA1', '#FFED00', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '10~20', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("10-20대")
-plt.savefig("output/10-20_2023.png", dpi='figure')
-plt.show()
-
-# [109]
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '30대', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("30대")
-plt.savefig("output/30.png", dpi='figure')
-plt.show()
-
-# 세대별 남성 여성 지지도 차이
-# [114]
-df = pd.read_excel("data/102030th.xlsx")
-df.head()
-
-# [120]
-df['date'] = pd.to_datetime(df[['연도', '월']].astype(str).agg('-'.join, axis=1), format='%Y-%m')
-df.head()
-
-# [128]
-custom_palette = ['#E61E2B', '#004EA1', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '10_20_man', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("18-29세 남성", size = 26)
-ax.set_ylabel("%")
-plt.savefig("output/10_20_man.png", dpi='figure')
-plt.show()
-
-# [129]
-custom_palette = ['#E61E2B', '#004EA1', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '10_20_woman', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("18-29세 여성", size=26)
-ax.set_ylabel("%")
-plt.savefig("output/10_20_woman.png", dpi='figure')
-plt.show()
-
-# [132]
-custom_palette = ['#E61E2B', '#004EA1', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '30_man', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("30대 남성", size = 26)
-ax.set_ylabel("%")
-plt.savefig("output/30_man.png", dpi='figure')
-plt.show()
-
-# [134]
-custom_palette = ['#E61E2B', '#004EA1', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '30_woman', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("30대 여성", size=26)
-ax.set_ylabel("%")
-plt.savefig("output/30_woman.png", dpi='figure')
-plt.show()
-
-# 수도권
-# [11]
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-df = pd.read_excel('data/정당지지도_데이터.xlsx')
-df['date'] = pd.to_datetime(df[['연도', '주']].astype(str).agg('-W'.join, axis=1) + '-5', format='%Y-W%W-%w')
-df
-
-# [4]
-custom_palette = ['#E61E2B', '#004EA1', '#FFED00', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '서울', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("서울")
-plt.savefig("output/서울.png", dpi='figure')
-plt.show()
-
-# [13]
-custom_palette = ['#E61E2B', '#004EA1', '#FFED00', '#808080']
-
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(df, x = 'date', y = '인천/경기', hue = '정당', palette=custom_palette, errorbar=None)
-ax.set_title("인천/경기")
-plt.savefig("output/인천경기.png", dpi='figure')
-plt.show()
-
+if __name__ == "__main__":
+    main()
